abc40b80beaf213b19d9bf96ed07b025cf948eec
= <=>
2015-09-18 21:24:09 +0200

    added initials for cifer10 parser

---
+++ include/csvm/csvm_cifar10_parser.h
@@ -0,0 +1,24 @@
+#ifndef CSVM_CIFAR10_PARSER_H
+#define CSVM_CIFAR10_PARSER_H
+
+#include <vector>
+#include <iostream>
+
+#include "csvm_image.h"
+
+using namespace std;
+
+namespace csvm{
+   
+   class CIFAR10{
+      vector<Image> images;
+      
+      
+   public:
+      void loadImages(string dir);
+   }
+   
+   
+}
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -6,17 +6,17 @@ using namespace csvm;
 using namespace std;
 
 int main(int argc,char**argv){
-   ImageScanner is(15,100);  //window size, nPatches
-   is.setImage("lenna.png");  //load image into ImageScanner
+   //ImageScanner is(15,100);  //window size, nPatches
+   //is.setImage("lenna.png");  //load image into ImageScanner
  
    
-   is.scanImage();            //scan the image 
+  // is.scanImage();            //scan the image 
 
    
    Image im("lenna.png");
-   Image cl;
-   cl = im.getROI(256,256,50,50);
-   cl.exportImage("ROI.png");
+  // Image cl;
+   //cl = im.getROI(256,256,50,50);
+   //cl.exportImage("ROI.png");
  
    return 0;
 }
