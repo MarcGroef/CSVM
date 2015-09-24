483cf579a7aaa93361366b88b346b939e80f090d
marcgroef <marcgroefsema@gmail.com>
2015-09-24 09:40:46 +0200

    added to new SVM class

--- include/csvm/csvm_svm.h
+++ include/csvm/csvm_svm.h
@@ -1,26 +1,48 @@
 #ifndef CSVM_SVM_H
 #define CSVM_SVM_H
 
+#include "csvm_feature.h"
+
 
 using namespace std;
 namespace csvm{
+  
+  
+  
+  struct SVM_Settings{
+    double alpha; 
+    double beta;
+    double COST;
+    double D2;
+    double SVM_C;
+    double EPS;
+    double SIGMA;
+    double INIT_ALPHA;
+    int ALPHA_ITER;
+    int NR_REP1;
+    int NR_REP2;
+  
+  }
    
-   class SVM{
+  class SVM{
       
-      //State variables
-      double** alphaCoefficients;
-      double** kernelTest;
-      double** kernel_train;
-      double** y;
-      double* bias;
-      double* output;
+    //State variables
+    struct SVM_Settings;
+    
+    string parameterFile;
+    string train
       
-      string parameterFile;
-      string train
+  public:
+     
+     
+    void setSettings(double alpha, double beta, double COST, double D2, double SVM_C, double EPS,
+      double SIGMA, double INIT_ALPHA, int ALPHA_ITER, int NR_REP1, int NR_REP2);
+    
+    double classify(Feature feat);  //classify the feature and return output
+    void train(Feature feat);
+     
       
-      
-      
-   }
+  }
    
 }
 
