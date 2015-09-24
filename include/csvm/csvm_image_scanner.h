8d9a8aff99471b56bec584561f8d356b0758956c
= <=>
2015-09-24 16:44:59 +0200

    removed ImageScanner content

--- include/csvm/csvm_image_scanner.h
+++ include/csvm/csvm_image_scanner.h
@@ -17,23 +17,7 @@ namespace csvm{
 
 
    class ImageScanner{
-      Image image;                                //openCV Image container to hold an image
-      string imageDir;                          //directory of the image
       
-      
-      Image window;                               //the window sample
-      int winSize;
-      int winPosX;
-      int winPosY;
-
-      int nPatches;                             //number of patches that should be taken
-
-      vector< vector<double> > v_hogValues;      // vector of hog vectors
-
-      public:
-      ImageScanner(int wSize, int nPatches);     //contructor
-      void setImage(string filename);           //load image into ImageScanner
-      void scanImage();                         //scan the image
 
    };
 
--- src/csvm/csvm_image_scanner.cc
+++ src/csvm/csvm_image_scanner.cc
@@ -3,29 +3,3 @@
 using namespace csvm;
 using namespace std;
 
-ImageScanner::ImageScanner(int wSize,int nPatches){
-   winSize = wSize;
-   this->nPatches = nPatches;
-
-}
-
-void ImageScanner::setImage(string filename){
-   image.loadImage(filename);
-   imageDir = filename;
-
-}
-
-
-
-
-
-void ImageScanner::scanImage(){
-   //for(int patch=0;patch<nPatches;patch++){
-
-
-
-   //}
-   HOGDescriptor hog(9,4,4,32,16);
-   v_hogValues = hog.getHOG(image,0);
-   cout << "done Imagescan. "<< v_hogValues.size() << " HOGs obtained \n";
-}
