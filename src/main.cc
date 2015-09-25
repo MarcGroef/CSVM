3b66eac6c1d7864de96efc0e32ced42647803cc2
= <=>
2015-09-25 20:26:51 +0200

    updated readme and made standard task less heavy on RAM

--- src/main.cc
+++ src/main.cc
@@ -38,9 +38,9 @@ int main(int argc,char**argv){
    imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_1.bin");
    imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_2.bin");
    imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_3.bin");
-   imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_4.bin");
-   imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_5.bin");
-   imDirs.push_back("../datasets/cifar-10-batches-bin/test_batch.bin");
+  // imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_4.bin");
+  // imDirs.push_back("../datasets/cifar-10-batches-bin/data_batch_5.bin");
+  // imDirs.push_back("../datasets/cifar-10-batches-bin/test_batch.bin");
    
    //load cifar10
    c.dataset.loadCifar10("../datasets/cifar-10-batches-bin/batches.meta.txt",imDirs);
@@ -50,7 +50,7 @@ int main(int argc,char**argv){
    cout << nImages << " images loaded.\n";
    
    //measure cpu time
-   /*time_t time0 = clock();
+   time_t time0 = clock();
    
    for(size_t idx = 0; idx < nImages; ++idx){
       newPatches = scanner.scanImage(c.dataset.getImagePtr(0),8,8,1,1);
@@ -58,7 +58,7 @@ int main(int argc,char**argv){
    }
    //print number of patches and time difference
    cout << patches.size() << " patches collected! in " << (clock() - time0)/1000  << " ms\n";
-   */
+   
    
    return 0;
 }
