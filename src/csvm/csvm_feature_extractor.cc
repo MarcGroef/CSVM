4d50083cae42208f16239a8b2fe7792cb086a136
= <=>
2015-10-03 23:16:32 +0200

    clustering

---
+++ src/csvm/csvm_feature_extractor.cc
@@ -0,0 +1,17 @@
+#include <csvm/csvm_feature_extractor.h>
+
+using namespace std;
+using namespace csvm;
+
+FeatureExtractor::FeatureExtractor(){
+   settings.featureType = LBP;  
+}
+
+Feature FeatureExtractor::extract(Patch p){
+   switch(settings.featureType){
+      case LBP:
+         return lbp.getLBP(p,0);
+      
+   }
+   return lbp.getLBP(p,0);
+}
