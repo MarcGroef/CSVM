641d1182d079731048270a17b360c4019fb6f625
= <=>
2015-11-02 01:13:09 +0100

    added temp wrapper

--- swig/swig.sh
+++ swig/swig.sh
@@ -3,7 +3,8 @@ swig -c++ -python csvm.i
 g++ -O3 -fPIC -I/usr/include/python2.7 -I../include -c ../src/*/*.cc  experiment.cc csvm_wrap.cxx -lm
 
 g++ -shared *.o -o ../PSO/testers/_csvm.so
-rm *.o *.cxx
+rm *.o
+#rm *.cxx
 
 mv csvm.py ../PSO/testers/csvm.py
 cd ../PSO/testers/
