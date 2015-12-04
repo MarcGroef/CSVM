b457e0b553705960eeafdfd6e0bc15f1ce20b237
marcgroef <marcgroefsema@gmail.com>
2015-12-04 10:29:32 +0100

    finished csvm initials

--- include/csvm/csvm_conv_svm.h
+++ include/csvm/csvm_conv_svm.h
@@ -2,11 +2,12 @@
 #define CSVM_CONV_SVM_H
 
 #include <vector>
+#include <limits>
 #include <iostream>
 
 #include "csvm_codebook.h"
 #include "csvm_dataset.h"
-
+#include "csvm_image.h"
 using namespace std;
 namespace csvm{
 
@@ -24,7 +25,7 @@ namespace csvm{
       vector< vector<double> > weights;
       vector<double> biases;
       ConvSVMSettings settings;
-      double output(vector< vector<double> >& activations);
+      double output(vector< vector<double> >& activations, unsigned int svmIdx);
    public:
       void setSettings(ConvSVMSettings s);
       void train(vector< vector< vector<double> > >& activations, CSVMDataset* ds);
--- src/csvm/csvm_conv_svm.cc
+++ src/csvm/csvm_conv_svm.cc
@@ -29,17 +29,55 @@ using namespace csvm;
       
       unsigned int nData = activations.size();
       for(size_t svmIdx = 0; svmIdx < settings.nClasses; ++svmIdx){
-         for(size_t dIdx = 0; dIdx < nData; ++dIdx){
-            unsigned int label = ds->getImagePtr(dIdx)->getlabelId();
-            double yData = (label == svmIdx ? 1.0 : -1.0);
-            double out = output(activations[dIdx], svmIdx);
-            double dSlack = (yData * out >= 1 ? 0 : yData * activations[dIdx][0])
-            
+         
+         for(size_t itIdx; itIdx < settings.nIter; ++itIdx){
+            double sumSlack = 0;
+            for(size_t dIdx = 0; dIdx < nData; ++dIdx){
+         
+               unsigned int label = ds->getImagePtr(dIdx)->getLabelId();
+               double yData = (label == svmIdx ? 1.0 : -1.0);
+               double out = output(activations[dIdx], svmIdx);
+               double dSlack = 0;
+               double weightSum = 0;
+               sumSlack += yData - out;
+               //determine slack differential
+               if(yData * out < 1){
+                  for(size_t clIdx = 0; clIdx < settings.nCentroids; ++clIdx){
+                     dSlack += activations[dIdx][0][clIdx];
+                     weightSum += weights[svmIdx][clIdx];
+                     
+                  }
+                  dSlack *= yData;
+               }
+               
+               //update weights
+               for(size_t clIdx = 0; clIdx < settings.nCentroids; ++clIdx){
+                  weights[svmIdx][clIdx] -= settings.learningRate * (0.5 * weightSum + settings.CSVM_C * dSlack) ;
+               }
+               
+               double objective = 0;
+               for(size_t clIdx = 0; clIdx < settings.nCentroids; ++clIdx){
+                  objective += weights[svmIdx][clIdx] * weights[svmIdx][clIdx];
+               }
+               objective += settings.CSVM_C * sumSlack;
+               
+               cout << "CSVM " << svmIdx << ": Objective = " << objective << endl;               
+               
+            }
          }
-      ]
+      }
    }
    
    unsigned int ConvSVM::classify(vector< vector<double> >& activations){
-      
+      unsigned int maxLabel = 0;
+      double maxOut = numeric_limits<double>::min();
+      for(size_t svmIdx = 1; svmIdx < settings.nClasses; ++svmIdx){
+         double out = output(activations, svmIdx);
+         if(out > maxOut){
+            maxOut = out;
+            maxLabel = svmIdx;
+         }
+      }
+      return maxLabel;
       
    }
