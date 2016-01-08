7c77e7ed63bc69bb770e9ed1f8a8282ed9bbb669
marcgroef <marcgroefsema@gmail.com>
2016-01-08 22:09:20 +0100

    added scaling

--- src/csvm/csvm_dataset.cc
+++ src/csvm/csvm_dataset.cc
@@ -39,7 +39,9 @@ void CSVMDataset::loadMNIST(string mnistDir){
    
    //splitDatasetToClasses();
    //cout << "dataset split to classes\n";
-   
+   mnist.scaleData(32);
+   cout << "Rescaled!\n";
+
 }
 
 void CSVMDataset::loadDataset(string dataDir){
@@ -137,7 +139,7 @@ void CSVMDataset::splitDataset(){
    unsigned int nTestData = settings.nTestImages;
    
    if(nTrainData + nTestData > nData){
-      cout << "csvm::CSVMDataset::splitDataset() WARNING! amount of testData + amount of trainData > nData in dataset! Exitting..\n";
+      cout << "csvm::CSVMDataset::splitDataset() WARNING! amount of testData + amount of trainData > nData (" << nTrainData << " + " << nTestData << " > " << nData << ")in dataset! Exitting..\n";
       exit(0);
    }
    
