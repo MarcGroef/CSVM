6dad86a860876d9261fa09dd50232d5e3ee03d9b
marcgroef <marcgroefsema@gmail.com>
2016-02-25 15:40:46 +0100

    added mlp class

---
+++ include/csvm/csvm_mlp.h
@@ -0,0 +1,31 @@
+#ifndef CSVM_MLP_H
+#define CSVM_MLP_H
+
+#include <vector>
+#include "csvm_feature.h"
+
+using namespace std;
+
+namespace csvm{
+   
+ struct MLPSettings{
+    //add your settings variables here (stuff you want to set through the settingsfile)
+    unsigned int nHiddenUnits;
+ }
+   
+   
+   
+ class MLP{
+    //class variables
+    
+ public:
+    void train(vector<Feature>& randomFeatures);
+    
+    vector<double> getActivations(vector<Feature>& imageFeatures);
+    
+ };
+   
+}
+
+
+#endif
