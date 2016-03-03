417f536e942f5c8d123a6c615a0c30fc8c226c1e
Rogier <rogierderuijter@hotmail.com>
2016-03-03 23:37:02 +0100

    mlp map

--- mlp/mlp.cc
+++ mlp/mlp.cc
@@ -1,6 +1,25 @@
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+#include <string>
+
+//-------------variables-----------------------
+const int nInputNodes = 10;
+const int nHiddenNodes = 10;
+const int nOutputNodes = 10;
+
+//double learningRate = 0.2;	
 	
+double weightsInputHidden[nInputNodes][nHiddenNodes];
+double weightsHiddenOutput[nHiddenNodes][nOutputNodes];
+
+double input[nInputNodes];
+double hiddenActivation[nHiddenNodes];
+double actualOutput[nOutputNodes];
+double desiredOutput[nOutputNodes] = {1,0,0,0,0,0,0,0,0,0};
+
+//-----------endvariables------------------------	
+
 double fRand(double fMin, double fMax){
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
@@ -13,47 +32,58 @@ void randomizeWeights(int sizeRow,int sizeCollum, double array[10][10]){
 		}
 	}
 }
-double sumInputUnits(double* inputValue, double* inputWeights,int size){
-	double sumInputUnitsTimesWeights = 0;
+double sumUnits(double* inputValue, double* inputWeights,int size){
+	double sumUnitsTimesWeights = 0;
 	double b = inputWeights[0];
 	for(int i=0;i<size;i++)
 	{	
-		sumInputUnitsTimesWeights+=inputValue[i]*inputWeights[i];
+		sumUnitsTimesWeights+=inputValue[i]*inputWeights[i];
 	}
-	return sumInputUnitsTimesWeights;
+	return sumUnitsTimesWeights + b;
 }
 
 double activationFunction(double summedActivation){
-	return 1/(1+exp (-summedActivation) );
+	return 1/(1+exp(-summedActivation));
+}
+
+void feedforward(){
+	double summedActivation = 0;
+
+	for(int i = 0; i < nHiddenNodes;i++){
+	summedActivation += sumUnits(input,weightsInputHidden[i],nHiddenNodes);
+	hiddenActivation[i] = activationFunction(summedActivation);
+	}
+	
+for(int i = 0; i < nOutputNodes;i++){
+	summedActivation += sumUnits(hiddenActivation,weightsHiddenOutput[i],nOutputNodes);
+	actualOutput[i] = activationFunction(summedActivation);
+	}
+	
+}
+
+double errorFunction(){
+	double error = 0;
+	for (int i=0; i< nOutputNodes;i++){
+		error += (desiredOutput[i] - actualOutput[i])*(desiredOutput[i] - actualOutput[i]);
+	}
+	error *= 0.5;
+	return error;
 }
 
 int main( void )
 {
-const unsigned int nInputNodes = 10;
-const unsigned int nHiddenNodes = 10;
-const unsigned int nOutputNodes = 10;
-
-double learningRate = 0.2;	
-	
-double weightsInputHidden[nInputNodes][nHiddenNodes];
-double weightsHiddenOutput[nHiddenNodes][nOutputNodes];
-
-double input[nInputNodes];
-double hiddenActivation[nHiddenNodes];
-double ActualOutput[nOutputNodes];
-double desiredOutput[nOutputNodes] = {1,0,0,0,0,0,0,0,0,0};
-
-double summedActivation = 0;
-
+double error;
 for(int i = 0; i<nInputNodes;i++){
 	input[i] = (double)rand() / RAND_MAX;;
-	}
+}
 	
 randomizeWeights(nInputNodes, nHiddenNodes, weightsInputHidden);
 randomizeWeights(nHiddenNodes, nOutputNodes, weightsHiddenOutput);
 
-for(int i = 0; i < nHiddenNodes;i++){
-	summedActivation += sumInputUnits(input,weightsInputHidden[i],nHiddenNodes);
-	hiddenActivation[i] = activationFunction(summedActivation);
-	}
+feedforward();
+
+error = errorFunction();
+
+std::cout << error;
+return 0;
 }
