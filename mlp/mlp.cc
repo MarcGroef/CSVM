448a85eb2dead4ce7d70e8a26acc38cf1b04ebab
S2389835 <s2389835@fwn-bborg-edu-74-96.fmnsedu.rug.nl>
2016-03-04 15:00:57 +0100

    mlp map

--- mlp/mlp.cc
+++ mlp/mlp.cc
@@ -4,19 +4,24 @@
 #include <string>
 
 //-------------variables-----------------------
-const int nInputNodes = 10;
-const int nHiddenNodes = 10;
-const int nOutputNodes = 10;
+const int nInputNodes = 2 + 1;
+const int nHiddenNodes = 2;
+const int nOutputNodes = 1;
+const int rowSize = nHiddenNodes;
 
-//double learningRate = 0.2;	
-	
+double learningRate = 0.5;	
+
+//double biasHidden = 0;
+
 double weightsInputHidden[nInputNodes][nHiddenNodes];
 double weightsHiddenOutput[nHiddenNodes][nOutputNodes];
 
-double input[nInputNodes];
+//double input[nInputNodes];
+double input[4][2]={{1,1},{1,0},{0,1},{0,0}};
 double hiddenActivation[nHiddenNodes];
 double actualOutput[nOutputNodes];
-double desiredOutput[nOutputNodes] = {1,0,0,0,0,0,0,0,0,0};
+double desiredOutput[4] = {0,1,1,0};
+int indexInput = 0;
 
 //-----------endvariables------------------------	
 
@@ -25,78 +30,173 @@ double fRand(double fMin, double fMax){
     return fMin + f * (fMax - fMin);
 }
 
-void randomizeWeights(int sizeRow,int sizeCollum, double array[10][10]){
-	for(int i = 0; i < sizeRow;i++){
-		for(int j = 0; j < sizeCollum;j++){
+void randomizeWeightsInputHidden(double array[][nHiddenNodes]){
+	for(int i = 0; i < nInputNodes-1;i++){
+		for(int j = 0; j < nHiddenNodes;j++){
+			array[i][j] = fRand(-0.5,0.5);
+		}
+	}
+	
+	//set bias weight to zero
+	for(int i = 0; i < nHiddenNodes;i++){
+		array[nInputNodes-1][i] = 0;
+	}
+}
+
+void randomizeWeightsHiddenOutput(double array[][nOutputNodes]){
+	for(int i = 0; i < nHiddenNodes;i++){
+		for(int j = 0; j < nOutputNodes;j++){
 			array[i][j] = fRand(-0.5,0.5);
 		}
 	}
 }
-double sumUnits(double* inputValue, double* inputWeights,int size){
-	double sumUnitsTimesWeights = 0;
-	double b = inputWeights[0];
-	for(int i=0;i<size;i++)
-	{	
-		sumUnitsTimesWeights+=inputValue[i]*inputWeights[i];
-	}
-	return sumUnitsTimesWeights + b;
-}
+
 
 double activationFunction(double summedActivation){
+	//std::cout << "1/(1+exp(-summedActivation)): "  << 1/(1+exp(-summedActivation)) << std::endl;
 	return 1/(1+exp(-summedActivation));
 }
 
-double derivativeActivationFunction(double summedActivation){
-	return exp(summedActivation)/((exp(summedActivation) + 1)*(exp(summedActivation) + 1))
+double derivativeActivationFunction(double activationNode){
+	return (1 - activationNode)*activationNode;
+}
+double sumUnits(int indexHidden,int size, double** activation, double** weights){
+	double sumUnitsTimesWeights = 0;
+	//double b = iets
+	
+	for(int i=0;i<nInputNodes;i++){	
+		sumUnitsTimesWeights+=activation[indexInput][i]*weights[i][indexHidden];
 	}
-
+	//sumUnitsTimesWeights += b;
+	return sumUnitsTimesWeights;
+}
 void feedforward(){
 	double summedActivation = 0;
 
-	for(int i = 0; i < nHiddenNodes;i++){
-	summedActivation += sumUnits(input,weightsInputHidden[i],nHiddenNodes);
-	hiddenActivation[i] = activationFunction(summedActivation);
+	for(int i = 0; i<nHiddenNodes;i++){
+		for(int j=0;j<nInputNodes-1;j++){	
+			summedActivation = summedActivation + input[indexInput][j]*weightsInputHidden[j][i];
+		}
+		//use bias
+		summedActivation += weightsInputHidden[nInputNodes-1][i] * desiredOutput[indexInput];	
+		hiddenActivation[i] = activationFunction(summedActivation);
+		summedActivation = 0;
 	}
 	
-for(int i = 0; i < nOutputNodes;i++){
-	summedActivation += sumUnits(hiddenActivation,weightsHiddenOutput[i],nOutputNodes);
-	actualOutput[i] = activationFunction(summedActivation);
+	for(int i = 0; i<nOutputNodes;i++){
+		for(int j=0;j<nHiddenNodes;j++){	
+			summedActivation = summedActivation + hiddenActivation[j]*weightsHiddenOutput[j][i];
+		}
+		//biasHidden *= 
+		//summedActivation += bias;
+		actualOutput[i] = activationFunction(summedActivation);
+		summedActivation = 0;
 	}
-	
 }
-
+//This function only works for one output node.
 double errorFunction(){
 	double error = 0;
 	for (int i=0; i< nOutputNodes;i++){
-		error += (desiredOutput[i] - actualOutput[i])*(desiredOutput[i] - actualOutput[i]);
+		error += (desiredOutput[indexInput] - actualOutput[i])*(desiredOutput[indexInput] - actualOutput[i]);
 	}
 	error *= 0.5;
 	return error;
 }
-//adjust weights with gradient decent
-void gradientDecent(double error){
-	deltaI = 0;
+//adjust weights with gradient decent, also only for one output node.
+void adjustWeightsOutputUnits(){
+	double deltaI = 0;
 	for(int i = 0; i < nOutputNodes;i++){
-		deltaI = 
-		
+		deltaI = (desiredOutput[indexInput] - actualOutput[i]) * derivativeActivationFunction(actualOutput[i]);
+		for(int j = 0; j < nHiddenNodes; j++){
+			weightsHiddenOutput[j][i] += learningRate * deltaI * hiddenActivation[j];
 		}
-	
+	}
 }
 
+void adjustWeightsHiddenUnit(){
+	double deltaO = 0;
+	double deltaI = 0;
+	double sumDeltaOWeights = 0;
+	
+	for(int i = 0; i < nHiddenNodes;i++){
+		for(int j = 0; j < nOutputNodes; j++){
+			deltaO = (desiredOutput[indexInput] - actualOutput[j]) * derivativeActivationFunction(actualOutput[j]);
+			sumDeltaOWeights += deltaO * weightsHiddenOutput[i][j];
+		}
+		deltaI = derivativeActivationFunction(hiddenActivation[i]) * sumDeltaOWeights;
+		sumDeltaOWeights = 0;
+		
+		for(int k = 0; k < nInputNodes-1; k++){
+			weightsInputHidden[k][i] += learningRate * deltaI * input[indexInput][k];
+		}
+		//adjust bias
+		weightsInputHidden[nInputNodes-1][i] += learningRate * deltaI * desiredOutput[indexInput];
+	}
+}
+
+void backpropgation(){
+	adjustWeightsHiddenUnit();
+	adjustWeightsOutputUnits();
+}
 int main( void )
 {
-double error;
-for(int i = 0; i<nInputNodes;i++){
-	input[i] = (double)rand() / RAND_MAX;;
-}
+	double error;
+	int epochs = 1000000;
+
+	randomizeWeightsInputHidden(weightsInputHidden);
+	randomizeWeightsHiddenOutput(weightsHiddenOutput);
+	 for (int i = 0; i < nInputNodes; ++i)
+    {
+        for (int j = 0; j < nHiddenNodes; ++j)
+        {
+            std::cout << weightsInputHidden[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    
+    std::cout << std::endl;
+     for (int i = 0; i < nHiddenNodes; ++i)
+    {
+        for (int j = 0; j < nOutputNodes; ++j)
+        {
+            std::cout  << weightsHiddenOutput[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
 	
-randomizeWeights(nInputNodes, nHiddenNodes, weightsInputHidden);
-randomizeWeights(nHiddenNodes, nOutputNodes, weightsHiddenOutput);
-
-feedforward();
-
-error = errorFunction();
-
-std::cout << error;
-return 0;
+	
+	//some epochs loop
+	for(int i = 0; i < epochs;i++){
+		indexInput = rand()%4;
+		std::cout << "indexInput: "  << indexInput << std::endl;
+		feedforward();
+		error = errorFunction();
+		std::cout << "error1: "  << error << std::endl;
+		backpropgation();
+		std::cout << "actualOutput[0]: "  << actualOutput[0] << std::endl;
+		std::cout << "desiredOutput[indexInput]: "  << desiredOutput[indexInput] << std::endl;
+		std::cout << std::endl;
+	}
+	
+	
+	
+	 for (int i = 0; i < nInputNodes; ++i)
+    {
+        for (int j = 0; j < nHiddenNodes; ++j)
+        {
+            std::cout << weightsInputHidden[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    
+     for (int i = 0; i < nHiddenNodes; ++i)
+    {
+        for (int j = 0; j < nOutputNodes; ++j)
+        {
+            std::cout << weightsHiddenOutput[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+	
+	return 0;
 }
