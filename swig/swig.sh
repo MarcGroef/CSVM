f5b5904d57a415df7cb80fb94e1deca1693d6f81
Marc Groefsema <marcgroefsema@gmail.com>
2016-04-22 22:58:37 +0200

    updated PALLAS

--- swig/swig.sh
+++ swig/swig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swig -c++ -python csvm.i
-g++ -Wall -O2 -fPIC -I/usr/include/python2.7 -I../include -c ../src/*/*.cc  experiment.cc csvm_wrap.cxx -lm
+g++ -Wall -std=c++11 -O2 -fPIC -I/usr/include/python2.7 -I../include -c ../src/*/*.cc  experiment.cc csvm_wrap.cxx -lm
 
 g++ -Wall -shared *.o -o ../PSO/testers/_csvm.so
 rm *.o
